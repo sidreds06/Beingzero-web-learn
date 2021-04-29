1f76cb55e457bda7a4db82843564955de29e5954
Sid <sidreds06@gmail.com>
2021-04-29 10:52:09 +0530

    adding html file

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");
+
+app.get("/resume", function(req, res){
+    res.send(/resume.html);    
 })
 
 // Heroku will automatically set an environment variable called PORT
