15e5065195b724d0f4cb8fd3dd8a9ccb23e26add
Sid <sidreds06@gmail.com>
2021-04-29 10:02:00 +0530

    git check 2

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Welcome to My Basic Site");
-    console.log("checkig git");
+    res.send("Welcome to My Basic Site"+" .....");
 })
 
 // Heroku will automatically set an environment variable called PORT
