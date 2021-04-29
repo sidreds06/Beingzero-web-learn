8150c5a09b469e5d34dc01dba09a0650d19b10a6
sidreds06 <52597261+sidreds06@users.noreply.github.com>
2021-04-29 11:10:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Welcome to My Basic Site");
-
-//app.get('/resume', function(req, res){
- //   res.sendFile('./resume.html');    
-})
-app.get('/resume', function(req, res){
+//app.get("/", function(req, res){
+  //  res.send("Welcome to My Basic Site");  
+//})
+app.get('/', function(req, res){
     res.sendFile('resume.html'); 
 })    
 // Heroku will automatically set an environment variable called PORT
