5dac59f6971de52176413f4c2dcecec64d26ed28
Sid <sidreds06@gmail.com>
2021-04-30 10:18:10 +0530

    edit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.get("/", function(req, res){
     res.send("Welcome to My Basic Site");  
 })
