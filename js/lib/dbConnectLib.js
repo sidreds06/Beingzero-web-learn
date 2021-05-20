7cf537b30adc3a223d9d0d143db2a7b4408c68ee
Sid <sidreds06@gmail.com>
2021-05-20 11:37:36 +0530

    edit

--- js/lib/dbConnectLib.js
+++ js/lib/dbConnectLib.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const config = require('../config/config')
 
-
+const connectionString="mongodb+srv://tenor06:Abhishyant22102004@cluster0.mw1u9.mongodb.net/CRUD?retryWrites=true&w=majority"
 
 module.exports={
 
     connect: function(){
-        mongoose.connect(config.connectionString,{})
+        mongoose.connect(connectionString,{})
 .then((result)=> console.log("Database Connected"))
 .catch((err)=>console.log("ERROR!!"))
 mongoose.connection.on('connected',function(){
